33011a314952238b9714a20fea6fc4d97767a059
adam <adammontemurro@gmail.com>
2023-04-03 14:50:30 -0400

    redeploy

--- config/config.js
+++ config/config.js
@@ -1,23 +1,49 @@
-{
-  "development": {
-    "username": "postgres",
-    "password": "1234",
-    "database": "group_goal",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+require('dotenv').config()
+module.exports = {
+  development: {
+    database: 'group_goal',
+    dialect: 'postgres'
   },
-  "test": {
-    "username": "postgres",
-    "password": "1234",
-    "database": "group_goal",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    database: 'group_goal',
+    dialect: 'postgres'
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "group_goal",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    }
   }
 }
+
+
+
+
+
+// {
+//   "development": {
+//     "username": "postgres",
+//     "password": "1234",
+//     "database": "group_goal",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   },
+//   "test": {
+//     "username": "postgres",
+//     "password": "1234",
+//     "database": "group_goal",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "group_goal",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   }
+// }
