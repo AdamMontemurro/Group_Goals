e61a706d40cc8aea721236e4f33c3d49b727081d
adam <adammontemurro@gmail.com>
2023-04-03 13:12:08 -0400

    resized logo image

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -19,7 +19,7 @@ const handleLogOut = () => {
   return (
     <div className='header'>
       <div style={{ cursor: "pointer" }} onClick={()=>navigate('/')} >
-      <img src="/static/media/LElogo.99ac6fe7.png" alt="logo" ></img>
+      <img className="logo-image" src="https://i.imgur.com/5RIIekP.png" alt="logo" ></img>
       </div>
       <div className='userArea'>
         {user ? <h4>Welcome! {user.user.name}</h4> : null}
