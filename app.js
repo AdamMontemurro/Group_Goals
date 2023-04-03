1d4d859b4a197a05ce307b60df61f1df5db4ea32
adam <adammontemurro@gmail.com>
2023-04-03 15:17:17 -0400

    connecting front end

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const app = express()
-const UserRouter= require('./routes/UserRouter')
+const UserRouter = require('./routes/UserRouter')
 const GroupRouter = require('./routes/GroupRouter')
 const TaskRouter = require('./routes/TaskRouter')
 
@@ -12,11 +12,16 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/', (req, res) => res.json({ message: 'Group Goal' }))
 
 app.use('/users', UserRouter)
 app.use('/groups', GroupRouter)
 app.use('/tasks', TaskRouter)
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
