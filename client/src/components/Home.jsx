345badb2732eba34da3ecf869bc0ae8c28af68f2
adam <adammontemurro@gmail.com>
2023-04-04 11:44:52 -0400

    added developmental roadmap to homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,6 +10,16 @@ const Home = ({user}) => {
       <h2>Group Goal</h2>
       <p> Welcome to Group Goal. The app designed to help you stay productive.</p>
       <p>All our tools are free to use right inside your browser!</p>
+      
+      <div className='roadmap-div'>
+      <p>Development Road Map:</p>
+      <ul className='roadmap'>
+        <li>Full Group Implimentation including group chat</li>
+        <li>ChatGPT integration</li>
+        <li>Voice Memos</li>
+      </ul>
+      <p>Send suggestions to <span className='users-name'>GroupGoalApp@gmail.com</span></p>
+      </div>
     </div>
   )
 }
